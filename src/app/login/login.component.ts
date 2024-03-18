a0d52935d3962d96879ce222898fe43fcf1f7282
carolnepomuceno <carolnepomuceno@gmail.com>
2024-03-18 10:06:19 -0300

    Meu projeto de angular

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { User } from '../models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  userModel = new User ("","")
+  receberDados(){
+    console.log(this.userModel)
+  }
+}
